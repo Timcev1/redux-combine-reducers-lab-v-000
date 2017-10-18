b770563d0738832a3f2d46c3f06481fa0205db11
Timothy Cevallos <timcev@gmail.com>
2017-10-18 19:15:28 +0000

    automatically backed up by learn

--- src/index.js
+++ src/index.js
@@ -1,4 +1,13 @@
 
 import { combineReducers } from 'redux'
- 
+export function combineReducers(reducers){
+  return (state = {}, action) => {
+    return Object.keys(reducers).reduce(
+      (nextState, key)=>{
+        nextState[key] = reducers[key](state[key], action);
+        return nextState
+      }, {}
+    )
+  }
+}
 const rootReducer = combineReducers({books, recommendedBooks})
