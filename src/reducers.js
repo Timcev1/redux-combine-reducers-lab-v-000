5021e4791ebc58f15ca0d3e85ea5774db0d3dde0
Timothy Cevallos <timcev@gmail.com>
2017-10-18 19:27:45 +0000

    automatically backed up by learn

--- src/reducers.js
+++ src/reducers.js
@@ -22,3 +22,13 @@ export function recommendedBooks(state = [], action){
     return state
   }
 }
+export function combineReducers(reducers){
+  return (state = {}, action) => {
+    return Object.keys(reducers).reduce(
+      (nextState, key)=>{
+        nextState[key] = reducers[key](state[key], action);
+        return nextState
+      }, {}
+    )
+  }
+}
