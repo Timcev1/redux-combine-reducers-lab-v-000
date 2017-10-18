ca0f3c9feab7c3e432a5c0bff3cdc7cf1472b706
Timothy Cevallos <timcev@gmail.com>
2017-10-18 19:21:54 +0000

    automatically backed up by learn

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
 import { combineReducers } from 'redux'
- 
+
 const rootReducer = combineReducers({books, recommendedBooks})
  
--- src/reducers.js
+++ src/reducers.js
@@ -22,6 +22,3 @@ export function recommendedBooks(state = [], action){
     return state
   }
 }
-import { combineReducers } from 'redux'
-
-const rootReducer = combineReducers({books, recommendedBooks})
